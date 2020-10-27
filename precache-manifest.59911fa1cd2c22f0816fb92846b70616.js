f37c6eb1c0fb8eb74b9941a54af75886252006fb
PrakashSharma2000 <ps4687675@gmail.com>
2020-10-27 16:12:52 +0530

    Updates

---
+++ precache-manifest.59911fa1cd2c22f0816fb92846b70616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d02d3856864245069ead56a269431b",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "fd28e97fc5a53c434f20",
+    "url": "/todoApp/static/css/main.dbbbde2a.chunk.css"
+  },
+  {
+    "revision": "5254299849f4fc0472f4",
+    "url": "/todoApp/static/js/2.118e130a.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/todoApp/static/js/2.118e130a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd28e97fc5a53c434f20",
+    "url": "/todoApp/static/js/main.47a4d8b9.chunk.js"
+  },
+  {
+    "revision": "7e87c45c590360734531",
+    "url": "/todoApp/static/js/runtime-main.ab082f0b.js"
+  }
+]);
